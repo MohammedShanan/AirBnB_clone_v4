703cf82978b9cae1a1ed841a6b08604c329d9443
mohammedshanan <mohammedshana12@gmail.com>
2024-08-10 19:50:39 +0300

    solved Fetch places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  let amenitiesObj = {};
+  $(".amenities input").click(function (event) {
+    let amenity = $(event.target);
+    if (amenity.is(":checked")) {
+      amenitiesObj[amenity.data("id")] = amenity.data("name");
+    } else {
+      delete amenitiesObj[amenity.data("id")];
+    }
+    amenitiesList = Object.values(amenitiesObj);
+    $(".amenities h4").text(amenitiesList.join(", "));
+  });
+
+  $.get("http://0.0.0.0:5001/api/v1/status/", function (data, status) {
+    if (status !== 200) {
+      $("#api_status").removeClass("available");
+    } else {
+      if (data.status === "OK") {
+        $("#api_status").addClass("available");
+      } else {
+        $("#api_status").removeClass("available");
+      }
+    }
+  });
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:5001/api/v1/places_search/",
+    contentType: "application/json",
+    data: JSON.stringify({}),
+    success: function (response) {
+      console.log(response);
+      let article = "";
+      response.forEach(function (place) {
+        article = `<article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">
+              ${place.max_guest} Gest${place.max_guest != 1 ? "s" : ""}
+            </div>
+            <div class="number_rooms">
+              ${place.number_rooms} Bedroom${place.number_rooms != 1 ? "s" : ""}
+            </div>
+            <div class="number_bathrooms">
+              ${place.number_bathrooms} Bathroom${
+          place.number_bathrooms != 1 ? "s" : ""
+        }
+            </div>
+          </div>
+        </article>`;
+        $(".places").append(article);
+      });
+    },
+  });
+});
